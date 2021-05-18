8dff03a714ca67107164b4e61dfdb642aa58eaf5
Czino <42438775+Czino@users.noreply.github.com>
2021-05-18 13:11:21 +0200

    Add information about block space usage

--- txperchannel.sh
+++ txperchannel.sh
@@ -1,5 +1,33 @@
+MINIMUM_TX_SIZE=109 # in virtual bytes, assuming no change output
+MINIMUM_TX_SIZE_CHANNEL_OPENING=121 # in virtual bytes, assuming no change output
+KILOBYTE=1024
+MEGABYTE=$(expr 1024 \* 1024)
+
 payments=$(lncli listpayments | jq -r '.payments' | jq length)
 channels=$(lncli listchannels | jq -r '[.channels[] | select(.initiator == true)]' | jq -r length)
-txPerChannelOpening=$(expr $payments / $channels)
+closedChannels=$(lncli closedchannels | jq -r '[.channels[] | select(.open_initiator == "INITIATOR_LOCAL")]' | jq -r length)
+let onchainTx="$channels"+"$closedChannels"
+let txPerOnchainTx="$payments"/"$onchainTx"
+let minimumSpaceUsed="$MINIMUM_TX_SIZE_CHANNEL_OPENING"*"$onchainTx"
+let minimumSpaceSaved="$MINIMUM_TX_SIZE"*"$payments"
 
-echo "You opened $channels channels and made $payments payments which results in $txPerChannelOpening transactions per on-chain transaction."
+if [ "$minimumSpaceUsed" -ge "$MEGABYTE" ]; then
+    minimumSpaceUsed=$(($minimumSpaceUsed / $MEGABYTE))
+    minimumSpaceUsed=$(echo "$minimumSpaceUsed vMB")
+elif [ "$minimumSpaceUsed" -ge "$KILOBYTE" ]; then
+    minimumSpaceUsed=$(($minimumSpaceUsed / $KILOBYTE))
+    minimumSpaceUsed=$(echo "$minimumSpaceUsed vKB")
+else
+    minimumSpaceUsed=$(echo "$minimumSpaceUsed vB")
+fi
+if [ "$minimumSpaceSaved" -ge "$MEGABYTE" ]; then
+    minimumSpaceSaved=$(($minimumSpaceSaved / $MEGABYTE))
+    minimumSpaceSaved=$(echo "$minimumSpaceSaved vMB")
+elif [ "$minimumSpaceSaved" -ge "$KILOBYTE" ]; then
+    minimumSpaceSaved=$(($minimumSpaceSaved / $KILOBYTE))
+    minimumSpaceSaved=$(echo "$minimumSpaceSaved vKB")
+else
+    minimumSpaceSaved=$(echo "$minimumSpaceSaved vB")
+fi
+echo "You opened $channels channels, closed $closedChannels channels and made $payments payments which results in $txPerOnchainTx transactions per online transaction"
+echo "You used at least $minimumSpaceUsed blockspace but saved at least $minimumSpaceSaved"
